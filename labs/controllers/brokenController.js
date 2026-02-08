9b083e16234fb4d1903270c373986e8e58e8ae5a
Vergil73 <jl6892139@gmail.com>
2026-02-08 15:41:04 +0545

    Lab 1 of Broken access controle is somewhat finished...

--- labs/controllers/brokenController.js
+++ labs/controllers/brokenController.js
@@ -1,6 +1,19 @@
+// Lab 1
+function attacker(req, res) {
+    const users = {
+        attacker: { role: 'attacker' },
+        victim: { role: 'victim', secret: 'FLAG-12345' }
+    };
 
-function attacker(req, res){
-    res.render('labs/brokenAccessControl/attacker')
+    const id = req.query.id;
+    const user = users[id];
+
+
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    res.render(`labs/brokenAccessControl/${user.role}`, { user });
 }
 
 
--- labs/routes/brokenAccessControlRoutes.js
+++ labs/routes/brokenAccessControlRoutes.js
@@ -3,17 +3,23 @@ const routes = Router();
 
 // Attacker page controller
 const { attacker } = require('../controllers/brokenController');
+// const { victim } = require('../controllers/brokenController');
 
+
+// Broken Access Controller Home Page
 routes.get('/labs/broken-access-control', (req, res) => {
     res.render('labs/brokenAccessControl/brokenAccessControl');
 });
 
-// Labd 1
+// Lab 1
 routes.get('/labs/broken-access-control/1', (req, res) => {
     res.render('labs/brokenAccessControl/1')
 });
 
 // Attacker
 routes.get('/labs/broken-access-control/1/profile', attacker);
+
+// Victim
+// routes.get('/labs/broken-access-control/1/profile', victim);
 
 module.exports = routes;
